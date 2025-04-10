927a5d5bb650d2786f45730dd488611423e4f3b1
hariharen96 <hariharen1996@gmail.com>
2025-04-10 10:14:11 +0530

    feat: add indexes on department and joining_date column

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,5 +9,8 @@ create table if not exists employees(
     salary decimal(10,2) not null,
     location varchar(50) not null,
     created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+    updated_at timestamp default current_timestamp 
 );
+
+create index if not exists index_employees_department on employees(department);
+create index if not exists index_employees_joining_date on employees(joining_date);
